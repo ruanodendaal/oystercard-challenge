6b11515c75e5b94f99285ee8c216d0aeb575c14c
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-20 17:55:50 +0000

    Complete challenge #6: enforce maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
   attr_reader :balance
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail 'Cannot top up beyond £90' if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,6 +15,13 @@ describe Oystercard do
     it 'it tops up a card with a specified amount' do
       expect { oystercard.top_up(50) }.to change{ oystercard.balance }.by 50
     end
+
+    it 'should not allow balance to exceed 90' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      oystercard.top_up(maximum_balance)
+      message = "Cannot top up beyond £#{maximum_balance}"
+      expect { oystercard.top_up(50) }.to raise_error message
+    end
   end
 
 
