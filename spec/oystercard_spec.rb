2a124474d60412f4528d5d6ee4be37dcdbdaa57e
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-20 17:10:11 +0000

    Complete challenge #4: add a balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  describe '#initialization' do
+    it 'sets a balance of 0' do
+      oystercard = Oystercard.new
+      expect(oystercard.balance).to eq 0
+    end
+  end
 end
