6b9c487a2c00677cd7f60689ff792df5f2f1917c
Ruan Odendaal <ruanodendaal@gmail.com>
2017-03-22 10:41:58 +0000

    Complete challenge #13: create station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'station'
+
+describe Station do
+subject(:station) { described_class.new("Aldgate", 1) }
+
+    it 'takes a name argument' do
+      expect(station.name).to eq "Aldgate"
+    end
+
+    it 'takes a zone argument' do
+      expect(station.zone).to eq 1
+    end
+
+end
